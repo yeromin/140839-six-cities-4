1c8bddf5704eb69bd30a56c84edd740eac1f9ab5
Dima <applestock@MacBook-Pro-Apple.local>
2020-06-10 13:35:49 +0300

    fix gitignore (remove preventing to index WEB files)

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from '../main/main';
+
+const Main = () => {
+  return (
+    <React.Fragment>
+      <App 
+        placesToStay={13123}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Main;
